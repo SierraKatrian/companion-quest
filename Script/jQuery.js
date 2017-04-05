f505bb7e501c5484caded5aa7418e84fee34db98
davidasherwilson <davidwilson@Davids-MacBook-Pro.local>
2017-04-05 17:48:14 -0400

    updated jQuery.js

--- Script/jQuery.js
+++ Script/jQuery.js
@@ -44,8 +44,23 @@ $( document ).ready(function() {
 
     //TOOL TIP
 
-    $(document).ready(function(){
-        $('[data-toggle="tooltip"]').tooltip();
-    });
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+
+    //CREATE A GAME
+
+        $('#dungeon-world-character-panel').hide();
+        $('#apocalypse-world-character-panel').hide();
+
+        $('#apocalypse-theme-radio').click(function(){
+            $('#apocalypse-world-character-panel').slideToggle();
+            $('#dungeon-world-character-panel').hide();
+        });
+
+        $('#dungeon-theme-radio').click(function(){
+            $('#dungeon-world-character-panel').slideToggle();
+            $('#apocalypse-world-character-panel').hide();
+        });
 
 });
