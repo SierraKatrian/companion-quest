42e518a3db639c5cf41a6b9708d87136722b6351
davidasherwilson <davidwilson@Davids-MacBook-Pro.local>
2017-04-08 20:14:58 -0400

    another banner for gm and player portal

---
+++ View/dungeon-world-banner.php
@@ -0,0 +1,11 @@
+<?php
+?>
+
+<div class="portal-banner">
+    <script type='text/javascript'>$(".portal-banner").css("background-image", "url(Images/portal-banner-dungeonworld.jpg)");</script>
+    <div class="portal-banner-logo-container">
+        <h2><img class="portal-banner-logo" src="Images/dungeon-World-logo-white.svg" alt="dungeon world banner" /></h2>
+    </div>
+    <button type="button" class="btn btn-info btn-lg portal-rulebook-btn" data-toggle="modal" data-target="#DungeonModal"><span class="glyphicon glyphicon-file"></span> rulebook</button>
+    <?php include "Modals/rulebook-dungeonworld.php" ?>
+</div>
