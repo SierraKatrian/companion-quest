9504c08f4e97f04d9afbe5a9b3171624484841bd
davidasherwilson <davidwilson@Davids-MacBook-Pro.local>
2017-04-08 20:13:51 -0400

    banner for gm and player portal added

---
+++ View/apocalypse-world-banner.php
@@ -0,0 +1,8 @@
+<div class="portal-banner">
+    <script type='text/javascript'>$(".portal-banner").css("background-image", "url(Images/portal-banner-apocalypseworld.jpg)");</script>
+    <div class="portal-banner-logo-container">
+        <h2><img class="portal-banner-logo" src="Images/apocalypse-World-logo-white.svg" alt="apocalypse world banner" /></h2>
+    </div>
+    <button type="button" class="btn btn-info btn-lg portal-rulebook-btn" data-toggle="modal" data-target="#ApocalypseModal"><span class="glyphicon glyphicon-file"></span> rulebook</button>
+    <?php include "Modals/rulebook-apocalypseworld.php" ?>
+</div>
