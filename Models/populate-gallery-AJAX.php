dfbd51b1256f60521ab224de7a7253a822571996
davidasherwilson <davidwilson@Davids-MacBook-Pro.local>
2017-04-20 13:25:21 -0400

    map model added

---
+++ Models/populate-gallery-AJAX.php
@@ -0,0 +1,19 @@
+<?php
+
+    session_start();
+
+    $gameDetails = $_SESSION['gameDetails'];
+    $gameID = $gameDetails['id'];
+
+    require_once "DbConnect.php";
+    require_once "MapsDAO.php";
+
+    $db = DbConnect::getDB();
+    $MapsDAO = new MapsDAO($db);
+    $displayMaps = $MapsDAO->READ_Maps($gameID);
+
+    foreach($displayMaps as $imageArray){
+        echo '<button id='.$imageArray['id'].' class="col-md-3 map-thumb" type="submit" name="mapImageBtn" value="" style="background:url(data:image;base64,'.$imageArray['image'].'); background-size:cover; background-position: center center; border:none;"></button>';
+    }
+
+?>
