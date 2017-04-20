b2ee1793a0d3a55af552ccf9bed9cb74d3b8adc4
davidasherwilson <davidwilson@Davids-MacBook-Pro.local>
2017-04-20 19:19:01 -0400

    update to map js

--- Script/Maps/update-selected-map-AJAX.js
+++ Script/Maps/update-selected-map-AJAX.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
         $.getJSON("./Models/ShowMapAjax.php",function(data){
              console.log(data);
              $('#current-map').html('<img src="data:image;base64,' + data[0][3] + '">');
+             $('#game-room-map-image').html('<img src="data:image;base64,' + data[0][3] + '" class="game-room-map-img">');
              $('#portal-map-image').html('<img src="data:image;base64,' + data[0][3] + '" alt="map" class="map-image" style="width:100%">');
         })//end of getJSON function
 
